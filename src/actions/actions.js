bffff28cec5bfa73a356ee899fc4305c68ca5213
Santiago Barrera <santiago.barrerae@gmail.com>
2022-03-27 14:41:27 -0500

    fetch movido a actions

---
+++ src/actions/actions.js
@@ -0,0 +1,10 @@
+export function fetchCharacters(url, dispatch) {
+  fetch(url)
+  .then((response) => response.json())
+  .then((data) => {
+    let results = data.results
+    let info = data.info;
+    dispatch({ type: "ADD_CHARACTER", results , info });
+  })
+  .catch((e) => console.log(e));
+}
--- src/components/Characters.jsx
+++ src/components/Characters.jsx
@@ -1,18 +1,8 @@
 import React from "react";
 import { connect } from "react-redux";
+import { fetchCharacters } from "../actions/actions";
 
 const urlCharacters = "https://rickandmortyapi.com/api/character";
-
-function fetchCharacters(url, dispatch) {
-  fetch(url)
-  .then((response) => response.json())
-  .then((data) => {
-    let results = data.results
-    let info = data.info;
-    dispatch({ type: "ADD_CHARACTER", results , info });
-  })
-  .catch((e) => console.log(e));
-}
 
 const Characters = ({ characters, info, addCharacter, changePage }) => (
 
