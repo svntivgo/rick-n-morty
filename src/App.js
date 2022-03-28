efbfe746316fcd9fbb6e2d77c789208e3a1b3cfd
Santiago Barrera <santiago.barrerae@gmail.com>
2022-03-28 00:17:41 -0500

    titulo

--- src/App.js
+++ src/App.js
@@ -9,19 +9,22 @@ import { fetchCharacters } from './actions/actions';
 const urlCharacters = "https://rickandmortyapi.com/api/character";
 
 const App = () => (
-
-    useEffect(() => {
-      // fetchCharacters(urlCharacters, store.dispatch)
-    }, []),
-
+  useEffect(() => {
+    // fetchCharacters(urlCharacters, store.dispatch)
+  }, []),
+  (
     <Provider store={store}>
       <nav>
         <NavBar />
       </nav>
       <main>
+        <h1 className="main__title" id="main__title">
+          Wiky Morty
+        </h1>
         <View />
       </main>
     </Provider>
-)
+  )
+);
 
 export default App;
